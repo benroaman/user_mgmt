2f546996cfcb228c12be0efeb746be1e2a07dd05
Ben Roaman <benroaman@gmail.com>
2015-01-29 19:57:23 -0500

    implemented all functionality, practically no styling

---
+++ js/main.js
@@ -0,0 +1,171 @@
+var userList = ObjectStore();
+var addUserForm = document.querySelector('.new-user-form');
+var searchBar = document.querySelector('.search-bar');
+var azButton = document.querySelector('.a-z');
+var zaButton = document.querySelector('.z-a');
+
+addUserForm.addEventListener('submit', function (e) {
+  e.stopPropagation();
+  e.preventDefault();
+  createUser();
+})
+
+searchBar.addEventListener('keyup', function () {
+  searchUsers(document.querySelector('.search-bar').value.trim());
+})
+
+azButton.addEventListener('click', function() {
+  orderAscending();
+})
+
+zaButton.addEventListener('click', function() {
+  orderDescending();
+})
+
+  //--------------------------------//
+ // Here be sorting functionality! //
+//--------------------------------//
+
+function orderAscending() {
+  var userArray = userList.query();
+  userArray.sort( function (a, b) {
+    if (a.fullName() > b.fullName()) {
+      return 1;
+    }
+    if (a.fullName() < b.fullName()) {
+      return -1;
+    }
+    return 0;
+  })
+
+  clearList();
+
+  for (var i = 0; i < userArray.length; ++i) {
+    document.querySelector('.user-list').appendChild(newUserListing(userArray[i]));
+  }
+}
+
+function orderDescending() {
+  var userArray = userList.query();
+  userArray.sort( function (a, b) {
+    if (b.fullName() > a.fullName()) {
+      return 1;
+    }
+    if (b.fullName() < a.fullName()) {
+      return -1;
+    }
+    return 0;
+  })
+
+  clearList();
+
+  for (var i = 0; i < userArray.length; ++i) {
+    document.querySelector('.user-list').appendChild(newUserListing(userArray[i]));
+  }
+}
+
+  //---------------------------------//
+ // Below lies search functionality //
+//---------------------------------//
+
+function searchUsers (str) {
+  var userArray = userList.query();
+  clearList();
+
+  for(var i = 0; i < userArray.length; ++i) {
+    if (userArray[i].email.indexOf(str) >= 0 || userArray[i].fullName().indexOf(str) >= 0 ) {
+      document.querySelector('.user-list').appendChild(newUserListing(userArray[i]));
+    }
+  }
+}
+
+function clearList() {
+  while (document.querySelector('.user-list').firstChild) {
+    document.querySelector('.user-list').removeChild(document.querySelector('.user-list').firstChild);
+  }
+}
+
+  //--------------------------------------//
+ // Below lies user-adding functionality //
+//--------------------------------------//
+
+function createUser () {
+  var newUser = User({ firstName: document.querySelector('.first-name-input').value.trim(),
+                          lastName: document.querySelector('.last-name-input').value.trim(),
+                          email: document.querySelector('.email-input').value.trim() });
+  if (userList.add(newUser)) {
+    document.querySelector('.user-list').appendChild(newUserListing(newUser));
+  } else {
+    alert('User already exists, silly!');
+  }
+  resetUserInput();
+}
+
+function resetUserInput() {
+  document.querySelector('.first-name-input').value = '';
+  document.querySelector('.last-name-input').value = '';
+  document.querySelector('.email-input').value = '';
+}
+
+function newUserListing(user) {
+  var userListingContainer = newUserListingContainer();
+  var userListingName = newUserListingName(user);
+  var userListingEmail = newUserListingEmail(user);
+  var deleteButton = newDeleteButton(user);
+  userListingContainer.appendChild(deleteButton);
+  userListingContainer.appendChild(userListingName);
+  userListingContainer.appendChild(userListingEmail);
+  return userListingContainer;
+}
+
+function newUserListingContainer() {
+  var shellDiv = document.createElement('div');
+  var classAtt = document.createAttribute('class');
+  classAtt.value = 'user-listing-container';
+  shellDiv.setAttributeNode(classAtt);
+  return shellDiv;
+}
+
+function newUserListingName(user) {
+  var userListingName = document.createElement('span');
+  var classAtt = document.createAttribute('class');
+  classAtt.value = 'user-listing-name';
+  userListingName.setAttributeNode(classAtt);
+  userListingName.textContent = user.fullName();
+  return userListingName;
+}
+
+function newUserListingEmail(user) {
+  var userListingEmail = document.createElement('a');
+  var classAtt = document.createAttribute('class');
+  var hrefAtt = document.createAttribute('href');
+  classAtt.value = 'user-listing-email';
+  hrefAtt.value = 'mailto:' + user.email;
+  userListingEmail.setAttributeNode(classAtt);
+  userListingEmail.setAttributeNode(hrefAtt);
+  userListingEmail.textContent = user.email;
+  return userListingEmail;
+}
+
+function refreshList() {
+  userArray = userList.query();
+  clearList();
+  for (var i = 0; i < userArray.length; ++i) {
+    document.querySelector('.user-list').appendChild(newUserListing(userArray[i]));
+  }
+}
+
+function newDeleteButton (user) {
+  var deleteButton = document.createElement('button');
+  var classAtt = document.createAttribute('class');
+  classAtt.value = 'delete';
+  deleteButton.setAttributeNode(classAtt);
+  deleteButton.textContent = 'x';
+
+  deleteButton.addEventListener('click', function () {
+    userList.remove(user);
+    refreshList();
+  })
+
+  return deleteButton;
+}
--- js/user.js
+++ js/user.js
@@ -1,5 +1,40 @@
 // User constructs a new user object. For more details, see
 // https://github.com/tiy-durham-fe-2015/curriculum/tree/master/assignments/user_mgmt
 function User(spec) {
-  // TODO: implement the user class, and get user unit tests passing
+  validateSpec(spec);
+
+  return {
+
+    firstName: spec.firstName,
+    lastName: spec.lastName,
+    email: spec.email,
+
+    fullName: function() {
+      return this.firstName + ' ' + this.lastName;
+    },
+
+    equal: function(otherUser) {
+      return this.email === otherUser.email;
+    }
+
+  }
+}
+
+//**************************//
+function validateSpec(spec) {
+  if (!spec) {
+    throw 'no user info submitted';
+  }
+
+  if (!spec.firstName || spec.firstName.trim() === '') {
+    throw 'no first name submitted';
+  }
+
+  if (!spec.lastName || spec.lastName.trim() === '') {
+    throw 'no last name submitted';
+  }
+
+  if (!spec.email || spec.email.trim() === '') {
+    throw 'no email submitted';
+  }
 }
